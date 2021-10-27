3498cf71b18a5763258c66af741d156246006306
Etiene Dalcol <dalcol@etiene.net>
2021-10-27 16:29:43 +0200

    add comment about code adaptation

--- game/data_reader_test.py
+++ game/data_reader_test.py
@@ -1,8 +1,5 @@
 # Test for input reading class
 #
-# Adapdated from example by Facebook Research
-# https://github.com/facebookresearch/EGG/
-#
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
