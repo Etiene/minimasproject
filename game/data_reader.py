827f3c47b90dc1e457eac9a5937ab7d38e94da17
Etiene Dalcol <dalcol@etiene.net>
2021-10-27 16:29:27 +0200

    remove unused import

--- game/data_reader.py
+++ game/data_reader.py
@@ -6,8 +6,6 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-
-import numpy as np
 import torch
 from torch.utils.data import Dataset
 
